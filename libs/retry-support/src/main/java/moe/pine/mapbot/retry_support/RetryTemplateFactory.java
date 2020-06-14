dd408f35512755e29bd24c9665078e238345ca30
Pine Mizune <pinemz@gmail.com>
2020-06-14 20:41:15 +0900

    refactor: use RetryTemplateBuilder

--- libs/retry-support/src/main/java/moe/pine/mapbot/retry_support/RetryTemplateFactory.java
+++ libs/retry-support/src/main/java/moe/pine/mapbot/retry_support/RetryTemplateFactory.java
@@ -6,6 +6,7 @@ import org.springframework.retry.policy.AlwaysRetryPolicy;
 import org.springframework.retry.policy.ExceptionClassifierRetryPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
+import org.springframework.retry.support.RetryTemplateBuilder;
 
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -14,40 +15,39 @@ import java.util.stream.Stream;
 public class RetryTemplateFactory {
     @SafeVarargs
     public static RetryTemplate create(
-        int maxAttempts,
-        long initialInterval,
-        double multiplier,
-        Class<? extends Throwable>... exceptions
+            int maxAttempts,
+            long initialInterval,
+            double multiplier,
+            Class<? extends Throwable>... exceptions
     ) {
         Map<Class<? extends Throwable>, Boolean> retryableExceptions =
-            Stream.of(exceptions)
-                .map(exception -> Map.entry(exception, true))
-                .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+                Stream.of(exceptions)
+                        .map(exception -> Map.entry(exception, true))
+                        .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
         RetryPolicy retryPolicy = new SimpleRetryPolicy(maxAttempts, retryableExceptions);
 
         ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
         backOffPolicy.setInitialInterval(initialInterval);
         backOffPolicy.setMultiplier(multiplier);
 
-        RetryTemplate retryTemplate = new RetryTemplate();
-        retryTemplate.setRetryPolicy(retryPolicy);
-        retryTemplate.setBackOffPolicy(backOffPolicy);
-
-        return retryTemplate;
+        return new RetryTemplateBuilder()
+                .customPolicy(retryPolicy)
+                .customBackoff(backOffPolicy)
+                .build();
     }
 
     @SafeVarargs
     public static RetryTemplate createUnlimited(
-        long initialInterval,
-        long maxInterval,
-        double multiplier,
-        Class<? extends Throwable>... exceptions
+            long initialInterval,
+            long maxInterval,
+            double multiplier,
+            Class<? extends Throwable>... exceptions
     ) {
         AlwaysRetryPolicy alwaysRetryPolicy = new AlwaysRetryPolicy();
         Map<Class<? extends Throwable>, RetryPolicy> policyMap =
-            Stream.of(exceptions)
-                .map(exception -> Map.entry(exception, alwaysRetryPolicy))
-                .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+                Stream.of(exceptions)
+                        .map(exception -> Map.entry(exception, alwaysRetryPolicy))
+                        .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
 
         ExceptionClassifierRetryPolicy retryPolicy = new ExceptionClassifierRetryPolicy();
         retryPolicy.setPolicyMap(policyMap);
@@ -57,10 +57,9 @@ public class RetryTemplateFactory {
         backOffPolicy.setMaxInterval(maxInterval);
         backOffPolicy.setMultiplier(multiplier);
 
-        RetryTemplate retryTemplate = new RetryTemplate();
-        retryTemplate.setRetryPolicy(retryPolicy);
-        retryTemplate.setBackOffPolicy(backOffPolicy);
-
-        return retryTemplate;
+        return new RetryTemplateBuilder()
+                .customPolicy(retryPolicy)
+                .customBackoff(backOffPolicy)
+                .build();
     }
 }
