619328722d2fe49cff8099b43de8c3f35a946539
Pine Mizune <pinemz@gmail.com>
2020-06-16 20:10:30 +0900

    fix: omit non HTML content

--- libs/medium/src/main/java/moe/pine/mapbot/medium/Browser.java
+++ libs/medium/src/main/java/moe/pine/mapbot/medium/Browser.java
@@ -4,23 +4,30 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 import moe.pine.mapbot.jsonld.JsonLd;
 import moe.pine.mapbot.jsonld.JsonLdParser;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
+import org.jsoup.UncheckedIOException;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RequiredArgsConstructor
 public class Browser {
     private static final Duration BLOCK_TIMEOUT = Duration.ofSeconds(30L);
+    private static final Set<MediaType> ALLOWED_MEDIA_TYPES =
+            Set.of(MediaType.TEXT_HTML, MediaType.APPLICATION_XHTML_XML);
 
     private final JsonLdParser jsonLdParser;
     private final WebClient webClient;
@@ -41,14 +48,30 @@ public class Browser {
             return Optional.empty();
         }
 
-        String content = clientResponse.bodyToMono(String.class)
-                .blockOptional()
-                .orElse(StringUtils.EMPTY);
-        if (StringUtils.isEmpty(content)) {
+        Optional<MediaType> mediaTypeOpt = clientResponse.headers().contentType();
+        if (mediaTypeOpt.isEmpty()) {
             return Optional.empty();
         }
 
-        Document document = Jsoup.parse(content);
+        MediaType mediaType = mediaTypeOpt.get();
+        if (!ALLOWED_MEDIA_TYPES.contains(mediaType)) {
+            log.debug("Unsupported media type [absoluteUrl={}, media-type={}]", absoluteUrl, mediaType);
+            return Optional.empty();
+        }
+
+        String content = clientResponse.bodyToMono(String.class).block();
+        if (StringUtils.isBlank(content)) {
+            return Optional.empty();
+        }
+
+        Document document;
+        try {
+            document = Jsoup.parse(content);
+        } catch (UncheckedIOException e) {
+            log.debug("Unable to parse HTML [absolute-url={}]", absoluteUrl, e);
+            return Optional.empty();
+        }
+
         Context context = new Context(document);
         return Optional.of(context);
     }
@@ -67,6 +90,7 @@ public class Browser {
                         .select("script[type=\"application/ld+json\"]")
                         .stream()
                         .map(Element::html)
+                        .filter(StringUtils::isNoneBlank)
                         .collect(Collectors.toUnmodifiableList());
 
         return jsonLdParser.parse(contents);
