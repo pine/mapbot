344a0a700a5c13fda3eeee66d99c1ae2f1487be1
Pine Mizune <pinemz@gmail.com>
2021-02-09 18:58:08 +0900

    chore: add debug log

--- app/src/main/java/moe/pine/mapbot/AppInitializer.java
+++ app/src/main/java/moe/pine/mapbot/AppInitializer.java
@@ -1,11 +1,18 @@
 package moe.pine.mapbot;
 
+import lombok.extern.slf4j.Slf4j;
 import moe.pine.heroku.addons.HerokuRedis;
 
+@Slf4j
 public final class AppInitializer {
     public static void run() {
+        String redisUrl = System.getenv("REDIS_URL");
+        log.info("REDIS_URL: {}", redisUrl);
+
         HerokuRedis redis = HerokuRedis.get();
         if (redis != null) {
+            log.info("HerokuRedis: {}", redis);
+
             System.setProperty("spring.redis.host", redis.getHost());
             System.setProperty("spring.redis.password", redis.getPassword());
             System.setProperty("spring.redis.port", Integer.toString(redis.getPort()));
