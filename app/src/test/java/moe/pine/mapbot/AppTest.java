f73ab2c06cba97d127bfdee0246f251cf73c4706
Pine Mizune <pinemz@gmail.com>
2021-04-09 10:37:28 +0900

    refactor: remove unnecessary annotation

--- app/src/test/java/moe/pine/mapbot/AppTest.java
+++ app/src/test/java/moe/pine/mapbot/AppTest.java
@@ -1,15 +1,12 @@
 package moe.pine.mapbot;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AppTest {
     @Autowired
